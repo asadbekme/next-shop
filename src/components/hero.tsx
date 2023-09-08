e84968cc0e846656e901da29636ed3699071411b
asadbekme <asadbekme2002@gmail.com>
2023-09-08 12:39:17 +0500

    fix: Image Optimization

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,11 +1,15 @@
+import Image from "next/image";
+
 const Hero = () => {
   return (
     <section className="text-gray-600 body-font mt-7">
       <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
         <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
-          <img
+          <Image
             className="object-cover object-center rounded"
             alt="hero"
+            width={500}
+            height={410}
             src="https://dummyimage.com/720x600"
           />
         </div>
--- src/components/product.tsx
+++ src/components/product.tsx
@@ -3,21 +3,18 @@
 import { FC } from "react";
 import { ProductType } from "@/interfaces";
 import Link from "next/link";
+import CustomImage from "./image";
 
 const Product: FC<{ product: ProductType }> = ({ product }) => {
-  console.log(product);
-
   return (
     <Link
       href={`/product/${product.id}`}
       className="h-96 flex flex-col p-6 rounded-lg hover:scale-105 transition-transform ease-out duration-200 border"
     >
-      <img
-        className="h-40 rounded w-full object-cover object-center mb-6"
-        src="https://dummyimage.com/720x400"
-        alt="content"
-      />
-      <h3 className="tracking-widest text-blue-700 text-xs font-medium">
+      <div className="relative flex-1 max-h-80">
+        <CustomImage product={product} fill />
+      </div>
+      <h3 className="tracking-widest text-blue-700 text-xs mt-5 font-medium">
         {product.category}
       </h3>
       <div className="font-semibold flex items-center justify-between mt-4 mb-2">
