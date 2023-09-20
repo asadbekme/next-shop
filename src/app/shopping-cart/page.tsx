0dcc6a06a497a0402e60a0fdcfe3788b6e901a97
asadbekme <asadbekme2002@gmail.com>
2023-09-20 15:25:48 +0500

    fix: style and tag

--- src/app/shopping-cart/page.tsx
+++ src/app/shopping-cart/page.tsx
@@ -50,7 +50,7 @@ const ShoppingCartPage = () => {
   };
 
   return (
-    <div className="bg-gray-100 pt-28">
+    <div className="bg-gray-100 pt-28 h-screen">
       <h1 className="mb-10 text-center text-2xl font-bold">Cart Items</h1>
       <div className="mx-auto max-w-5xl justify-center px-6 md:flex md:space-x-6 xl:px-0">
         <div className="rounded-lg md:w-2/3">
@@ -105,26 +105,26 @@ const ShoppingCartPage = () => {
                 </div>
                 <div className="mt-4 flex justify-between sm:space-y-6 sm:mt-0 sm:block sm:space-x-6">
                   <div className="flex items-center border-gray-100">
-                    <span
+                    <button
                       className="cursor-pointer rounded-l bg-gray-100 py-1 px-3.5 duration-100 hover:bg-blue-500 hover:text-blue-50"
                       onClick={() => handleDecrement(product.id)}
                     >
                       {" "}
                       -{" "}
-                    </span>
+                    </button>
                     <input
                       className="h-8 w-8 border bg-white text-center text-xs outline-none"
                       type="number"
                       value={product.quantity}
                       min="1"
                     />
-                    <span
+                    <button
                       className="cursor-pointer rounded-r bg-gray-100 py-1 px-3 duration-100 hover:bg-blue-500 hover:text-blue-50"
                       onClick={() => handleIncrement(product.id)}
                     >
                       {" "}
                       +{" "}
-                    </span>
+                    </button>
                   </div>
                   <div className="flex items-center space-x-4">
                     <p className="text-sm">
